b39e04094758c280500858473c997ce137e1161f
Mrinal Khemka <khemkamrinal69@gmail.com>
2020-05-23 14:27:49 +0530

    initial commit

---
+++ javascript/typewritter.js
@@ -0,0 +1,20 @@
+
+var messages=["A Designer","A Competitive Coder"];
+var rank=0;
+
+// Code for Chrome, Safari and Opera
+document.getElementById("myTypewriter").addEventListener("webkitAnimationEnd", changeTxt);
+
+// Standard syntax
+document.getElementById("myTypewriter").addEventListener("animationend", changeTxt);
+
+function changeTxt(e){
+  _h1 = this.getElementsByTagName("h1")[0];
+  _h1.style.webkitAnimation = 'none'; // set element animation to none
+   setTimeout(function() { // you surely want a delay before the next message appears
+      _h1.innerHTML=messages[rank];
+      var speed =3.5*messages[rank].length/20; // adjust the speed 
+      _h1.style.webkitAnimation = 'typing '+speed+'s steps(40, end), blink-caret .75s step-end infinite'; //  switch to the original set of animation      
+      (rank===messages.length-1)?rank=0:rank++; // if you have displayed the last message from the array, go back to the first one, else go to next message
+    }, 1000);
+}
